2c21faec5a9440f1942274c1138fdbca6d5243c0
AlexCavalcantiJordao <alex37705732@gmail.com>
2023-05-20 15:04:45 -0300

    Exercicios-Listas.dev Exercicios-Listas.layout Listas.cpp

---
+++ Listas.cpp
@@ -0,0 +1,30 @@
+#include <windows.h>
+
+#include <iostream>
+using namespace std;
+
+struct Node {
+  int data;
+  Node* next;
+};
+
+int main() {
+  Node* head = NULL;
+  Node* second = NULL;
+  Node* third = NULL;
+
+  head = new Node();
+  second = new Node();
+  third = new Node();
+
+  head->data = 1;
+  head->next = second;
+
+  second->data = 2;
+  second->next = third;
+
+  third->data = 3;
+  third->next = NULL;
+
+  return 0;
+}
